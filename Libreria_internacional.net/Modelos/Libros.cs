ba5af4b98a9b1ee8613366c366b7056af42c4e86
oscarCR19 <oscarmora2006@gmail.com>
2022-03-22 22:36:53 -0600

    Implementando MVC de forma manual

---
+++ Libreria_internacional.net/Modelos/Libros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libreria_internacional.net.Modelos
+{
+    public class Libros
+    {
+        public string ISBN { get; set; }
+        public string Foto { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public Double Precio { get; set; }
+    }
+}
---
+++ Libreria_internacional.net/Modelos/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libreria_internacional.net.Modelos
+{
+    public class Usuarios
+    {
+        public string Nombre { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
