a0935ef95255411714d3a8cc79b79a945ad2b4a0
oscarCR19 <oscarmora2006@gmail.com>
2022-04-04 20:49:52 -0600

    Creacion de las demas vistas, ingreso de algunas etiquetas html

---
+++ Libreria_internacional.net/Vistas/MiCarrito.aspx.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Libreria_internacional.net.Vistas
+{
+
+
+    public partial class MiCarrito
+    {
+
+        /// <summary>
+        /// Control theme_link.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlLink theme_link;
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+    }
+}
